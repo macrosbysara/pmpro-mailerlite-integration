5f8d4025c0ddd504aad75463cd42e05e4a414e2a
KJ Roelke <kj.roelke@gmail.com>
2026-05-10 15:27:44 -0500

    lint js

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,11 @@ import domReady from '@wordpress/dom-ready';
 import { createRoot } from '@wordpress/element';
 import App from './App';
 
-declare const mbsSettings: { nonce: string; restBase: string };
-
 domReady( () => {
 	const el = document.getElementById( 'mbs-settings' );
 	if ( ! el ) {
 		return;
 	}
 	const root = createRoot( el );
-	root.render(
-		<App />
-	);
+	root.render( <App /> );
 } );
